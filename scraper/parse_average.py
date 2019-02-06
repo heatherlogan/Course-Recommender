16d7c8171c6bad88ab01ffe3c0080c065d90ba4c
Nicole Shek <nicoleshek315@gmail.com>
2019-02-06 16:39:43 +0000

    modified parse_average.py and created textfile with course codes and their corresponding average

--- scraper/parse_average.py
+++ scraper/parse_average.py
@@ -6,10 +6,26 @@ def main():
     with open("course_result_summary.pdf", "rb") as f:
         pdf = pdftotext.PDF(f)
 
-    all_text = "\n".join(pdf).strip()
+    all_text = "".join(pdf).strip()
+    #print(all_text)
     split_by_line = all_text.split('\n')
-    print [re.split(r'\s{2,}', x) for x in split_by_line]
+    #print(split_by_line)
 
-    
+    # for s in split_by_line:
+    #     print(s)
+    course =  [re.split(r'\s{1,}', x) for x in split_by_line]
+
+    for c in course:
+        if c[0].startswith('INF'):
+            print(c[0], c[-2])
+
+    with open('average.txt', 'w') as f:
+        f.write('INFCODE\t' + 'AVERAGE\n')
+
+        for c in course:
+            if c[0].startswith('INF'):
+                f.write(c[0] + '\t' + c[-2] + '\n')
+
+
 if __name__ == '__main__':
     main()
